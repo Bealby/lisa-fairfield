8829032fa19b4acc85b68cc3020e2af6d2d477bb
Bealby <mgbealby@gmail.com>
2021-02-24 20:30:16 +0000

    Audio background color gradient

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,3 +1,5 @@
+// Allows only one audio to play at a time
+
 document.addEventListener('play', function(e){
     var audios = document.getElementsByTagName('audio');
     for(var i = 0, len = audios.length; i < len;i++){
